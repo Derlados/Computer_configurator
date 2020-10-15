f57234e1e3ae39c87fa91a6147129420c7f533c7
Illia <derladoshome@gmail.com>
2020-10-15 15:36:13 +0300

    created good blank

--- app/src/main/java/com/derlados/computerconf/MainActivity.java
+++ app/src/main/java/com/derlados/computerconf/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager pager=(ViewPager)findViewById(R.id.activity_main_pager);
-        pager.setAdapter(new MenuPageAdapter(getSupportFragmentManager()));
+//        ViewPager pager=(ViewPager)findViewById(R.id.activity_main_pager);
+//        pager.setAdapter(new MenuPageAdapter(getSupportFragmentManager()));
 
     }
 }
