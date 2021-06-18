42c18dd47c5dfe869c852e7569e649ba1a23e6e2
Illia <derladoshome@gmail.com>
2021-06-18 20:22:17 +0300

    update .gitignore

--- server/dist/models/data-classes/Attribute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Attribute = void 0;
-class Attribute {
-}
-exports.Attribute = Attribute;
-//# sourceMappingURL=Attribute.js.map
--- server/dist/models/data-classes/Component.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Component = void 0;
-class Component {
-}
-exports.Component = Component;
-//# sourceMappingURL=Component.js.map
